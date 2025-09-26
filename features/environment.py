e0022c5ffb255858938e1739536003ae68cfa92d
Alaeddine Chahbani <alaeddine.chahbani@gmail.com>
2025-09-26 15:29:39 +0200

    lancer les tests en headless

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,9 @@
-from selenium.webdriver import Chrome
+from selenium import webdriver
 
 def before_scenario(context, scenario):
-    context.driver = Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    context.driver = webdriver.Chrome()
     context.driver.maximize_window()
 
 def after_scenario(context, scenario):
